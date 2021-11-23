ed7989d9f0dc60d4488791cabb6c0a6320cc28fd
JoKwanhee <jonotch1@naver.com>
2021-11-23 19:49:52 +0900

    task5

---
+++ app/src/main/java/com/example/bcsd_task5/FragmentOne.kt
@@ -0,0 +1,28 @@
+package com.example.bcsd_task5
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class FragmentOne : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentData = inflater.inflate(R.layout.fragment_one, container, false)
+        val num = arguments?.getString("number")
+        val numInt = arguments?.getInt("number_int") ?: 0
+        val explanation = fragmentData.findViewById<TextView>(R.id.explanation_text)
+        val numberText = fragmentData.findViewById<TextView>(R.id.second_text_number)
+        val rand = java.util.Random()
+
+        explanation.text = "Here is a random number between 0 and $num"
+        numberText.text = rand.nextInt(numInt).toString()
+
+        return fragmentData
+    }
+}
